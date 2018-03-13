28343ef8cbfbaacb13aa2ad3c56c08c3cc5d8ee9
zhoupeng <zhoupeng@xjgreat.com>
2018-03-13 15:07:54 +0800

    add concerns type for future useage

--- src/constants.ts
+++ src/constants.ts
@@ -8,9 +8,13 @@ export const REL_JAVASCRIPTS = join("app","assets","javascripts");
 export const REL_STYLESHEETS = join("app","assets","stylesheets");
 export const REL_SPEC = "spec"
 export const REL_TEST = "test"
+export const REL_CONTROLLERS_CONCERNS = join("app","controllers","concerns");
+export const REL_MODELS_CONCERNS =  join("app","models","concerns");
 export enum FileType {
     Controller,
+    ControllerConcerns,
     Model,
+    ModelConcerns,
     Layout,
     View,
     Helper,
@@ -22,7 +26,9 @@ export enum FileType {
 }
 export var FileTypeRelPath = new Map([
     [FileType.Controller,REL_CONTROLLERS],
+    [FileType.ControllerConcerns,REL_CONTROLLERS_CONCERNS],
     [FileType.Model,REL_MODELS],
+    [FileType.ModelConcerns,REL_MODELS_CONCERNS],
     [FileType.Layout,REL_LAYOUTS],
     [FileType.View,REL_VIEWS],
     [FileType.Helper,REL_HELPERS],
