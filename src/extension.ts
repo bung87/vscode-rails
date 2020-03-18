dc2dc9aba3e2827eb910ec45d625e1bffb30180c
zhoupeng <zhoupeng@xjgreat.com>
2020-03-19 01:44:27 +0800

    clean code

--- src/extension.ts
+++ src/extension.ts
@@ -10,20 +10,16 @@ import { ViewDefinitionProvider } from "./viewRef";
 import lineByLine = require("n-readlines");
 import { viewDoc } from "./view_doc";
 import path = require('path');
-import {Formatter} from './formatter';
+import { Formatter } from './formatter';
 
 const RAILS_MODE: vscode.DocumentFilter = { language: "ruby", scheme: "file" };
 const VIEW_MODE: vscode.DocumentFilter = {
   pattern: "**/views/**",
   scheme: "file"
 };
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-function railsNavigation() {
-  // The code you place here will be executed every time your command is executed
 
-  // Display a message box to the user
-  //vscode.window.showInformationMessage('Hello World!');
+function railsNavigation() {
+
   if (!vscode.window.activeTextEditor) {
     return;
   }
@@ -40,65 +36,39 @@ function railsNavigation() {
   rh.showFileList();
 }
 
-function registerFormatter(context: vscode.ExtensionContext){
+function registerFormatter(context: vscode.ExtensionContext) {
   console.log("registerFormatter")
-  var docType: Array<string> = ['css.erb', 'scss.erb',  'html.erb'];
+  var docType: Array<string> = ['css.erb', 'scss.erb', 'html.erb'];
 
   for (var i = 0, l = docType.length; i < l; i++) {
-      registerDocType(docType[i]);
+    registerDocType(docType[i]);
   }
 
   let formatter = new Formatter();
 
   context.subscriptions.push(vscode.commands.registerCommand('erb.formatting', () => {
-      formatter.beautify();
+    formatter.beautify();
   }));
 
-
-  // context.subscriptions.push(vscode.commands.registerCommand('Lonefy.formatterConfig', () => {
-
-  //     formatter.openConfig(
-  //         path.join(vscode.workspace.rootPath || '.', '.vscode', 'formatter.json'),
-  //         function () {
-  //             vscode.window.showInformationMessage('[Local]  After editing the file, remember to Restart VScode');
-  //         },
-  //         function () {
-  //             var fileName = path.join(__dirname, 'formatter.json');
-  //             formatter.openConfig(
-  //                 fileName,
-  //                 function () {
-  //                     vscode.window.showInformationMessage('[Golbal]  After editing the file, remember to Restart VScode');
-  //                 },
-  //                 function () {
-  //                     vscode.window.showInformationMessage('Not found file: ' + fileName);
-  //                 })
-  //         })
-  // }));
-
-
-  // context.subscriptions.push(vscode.commands.registerCommand('Lonefy.formatterCreateLocalConfig', () => {
-  //     formatter.generateLocalConfig();
-  // }));
-
   context.subscriptions.push(vscode.workspace.onWillSaveTextDocument(e => {
-      formatter.onSave(e)
+    formatter.onSave(e)
   }));
 
 
   function registerDocType(type) {
 
-      context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(type, {
-          provideDocumentFormattingEdits: (document, options, token) => {
-              return formatter.registerBeautify(null)
-          }
-      }));
-      context.subscriptions.push(vscode.languages.registerDocumentRangeFormattingEditProvider(type, {
-          provideDocumentRangeFormattingEdits: (document, range, options, token) => {
-              var start = new vscode.Position(0, 0);
-              var end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
-              return formatter.registerBeautify(new vscode.Range(start, end))
-          }
-      }));
+    context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(type, {
+      provideDocumentFormattingEdits: (document, options, token) => {
+        return formatter.registerBeautify(null)
+      }
+    }));
+    context.subscriptions.push(vscode.languages.registerDocumentRangeFormattingEditProvider(type, {
+      provideDocumentRangeFormattingEdits: (document, range, options, token) => {
+        var start = new vscode.Position(0, 0);
+        var end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
+        return formatter.registerBeautify(new vscode.Range(start, end))
+      }
+    }));
   }
 }
 
@@ -158,4 +128,4 @@ export function activate(context: vscode.ExtensionContext) {
   });
 }
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
